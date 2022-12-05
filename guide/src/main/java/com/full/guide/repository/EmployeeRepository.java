4d68ede623b102a4586ca8b112ce692900cc4cf0
rag678 <bk2anurag@gmail.com>
2022-12-05 13:23:02 +0530

    Adding Backend to project

---
+++ guide/src/main/java/com/full/guide/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.full.guide.repository;
+
+import com.full.guide.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+}
---
+++ guide/src/test/java/com/full/guide/GuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.full.guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
